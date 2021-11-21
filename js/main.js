8950b8525e0e4598830966289ada823e757a37ca
robert-virtual <robertocastill63@gmail.com>
2021-11-21 17:52:32 -0600

    main js cambios

--- js/main.js
+++ js/main.js
@@ -2,17 +2,21 @@ const div = document.createElement("div");
 let current = document.createElement("a");
 const links = Array.from(document.querySelectorAll(".menu a"));
 
-document.addEventListener("change", ({ target }) => {
-  if (target.matches(".menu a")) {
-    current.classList.remove("active");
-    current = target;
-    target.classList.add("active");
-  }
-});
+// document.addEventListener("change", ({ target }) => {
+//   if (target.matches(".menu a")) {
+//     current.classList.remove("active");
+//     current = target;
+//     target.classList.add("active");
+//   }
+// });
 
 window.onhashchange = (e) => {
   console.log("change", location.hash);
-  console.log(links);
-  let a = links.find((e) => e.hash == location.hash);
-  console.log(a);
+  //   let a = links.find((e) => e.hash == location.hash);
+  links.forEach((e) => {
+    if (e.hash == location.hash) {
+      e.classList.add("active");
+      console.log(e);
+    }
+  });
 };
