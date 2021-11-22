3b9fb7c2df7676d90479e4b6a7dcc4527c3009de
robert-virtual <robertocastill63@gmail.com>
2021-11-22 15:05:30 -0600

    menu actualizado

--- js/main.js
+++ js/main.js
@@ -1,12 +1,26 @@
-const div = document.createElement("div");
 let current = document.createElement("a");
 const links = Array.from(document.querySelectorAll(".menu a"));
 let sections = document.querySelectorAll(".content section");
+console.log(links);
+console.log(sections);
+// the magic
 
-window.onhashchange = (e) => {
-  console.log("change", location.hash);
-  let a = links.find((e) => e.hash == location.hash);
-  current.classList.remove("active");
-  current = a;
-  a.classList.add("active");
+const callCack = (entries) => {
+  console.log(entries);
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      console.log("newcurrent:", entry.target.id);
+      current.classList.remove("active");
+      let newCurrent = links.find((link) => link.hash == "#" + entry.target.id);
+      newCurrent.classList.add("active");
+      current = newCurrent;
+    }
+  });
 };
+
+const observer = new IntersectionObserver(callCack, {
+  rootMargin: "-250px",
+});
+sections.forEach((sec) => {
+  observer.observe(sec);
+});
