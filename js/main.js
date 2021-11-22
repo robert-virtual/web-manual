249cb8b90f426b920e9e7421a7f274b7ab97d8bb
robert-virtual <robertocastill63@gmail.com>
2021-11-22 12:58:08 -0600

    agrege imagenes de clientes

--- js/main.js
+++ js/main.js
@@ -1,15 +1,7 @@
 const div = document.createElement("div");
 let current = document.createElement("a");
 const links = Array.from(document.querySelectorAll(".menu a"));
-let sections = document.createElement(".content section");
-
-// document.addEventListener("change", ({ target }) => {
-//   if (target.matches(".menu a")) {
-//     current.classList.remove("active");
-//     current = target;
-//     target.classList.add("active");
-//   }
-// });
+let sections = document.querySelectorAll(".content section");
 
 window.onhashchange = (e) => {
   console.log("change", location.hash);
